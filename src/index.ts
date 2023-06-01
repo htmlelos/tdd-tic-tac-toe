80b106bfd14f4b0abb51c8eadf2a2a45d864b9cb
Sergio Lucero <sergiolucero73@gmail.com>
2023-06-01 20:23:46 -0300

    feature: determina el estado del juego

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,21 @@
 export type Player = "X" | "O" | "";
 
+export type Status = "PLAYING" | "WON" | "DRAW";
+
 export type Board = Array<Player>;
 
 export type Game = {
   currentPlayer: Player;
   players: Player[];
   board: Board;
+  status: Status;
 };
 export const createGame = () => {
   return {
     currentPlayer: "X",
     players: ["X", "O"],
     board: Array(9).fill(""),
+    status: "PLAYING",
   } as Game;
 };
 
@@ -27,19 +31,82 @@ export const getCurrentPlayer = (game: Game) => {
   return game.currentPlayer;
 };
 
+export const hasHorizontalWin =
+  (player: Player) =>
+  (game: Game): boolean => {
+    const board = game.board;
+    const rows = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+    ];
+
+    return rows.some((row) => row.every((cell) => board[cell] === player));
+  };
+
+export const hasVerticalWin =
+  (player: Player) =>
+  (game: Game): boolean => {
+    const board = game.board;
+    const columns = [
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+    ];
+
+    return columns.some((column) =>
+      column.every((cell) => board[cell] === player)
+    );
+  };
+
+export const hasDiagonalLeftToRightWin =
+  (player: Player) =>
+  (game: Game): boolean => {
+    const board = game.board;
+    const diagonal = [0, 4, 8];
+
+    return diagonal.every((cell) => board[cell] === player);
+  };
+
+export const hasDiagonalRightToLeftWin =
+  (player: Player) =>
+  (game: Game): boolean => {
+    const board = game.board;
+    const diagonal = [2, 4, 6];
+
+    return diagonal.every((cell) => board[cell] === player);
+  };
+
 export const play = (position: number) => (game: Game) => {
-  if (position < 0 || position > 8) return game;
+  if (position < 0 || position > game.board.length) return game;
+  if (game.board[position] !== "") return game;
 
-  if (game.board[position] === "") {
-    return {
-      ...game,
-      board: updateBoard(position)(game),
-      currentPlayer: switchPlayer(game),
-    } as Game;
-  }
+  const player = game.currentPlayer === "X" ? "O" : "X";
+  const updateBoard = [...game.board];
+  updateBoard[position] = player;
 
-  return game;
+  const hasPlayerWon =
+    hasHorizontalWin(player)(game) ||
+    hasVerticalWin(player)(game) ||
+    hasDiagonalLeftToRightWin(player)(game) ||
+    hasDiagonalRightToLeftWin(player)(game);
+
+  const isGameTied = !hasPlayerWon && updateBoard.every((cell) => cell !== "");
+
+  const newStatus = hasPlayerWon
+    ? "WON"
+    : isGameTied
+    ? "DRAW"
+    : ("PLAYING" as Status);
+
+  return {
+    ...game,
+    board: updateBoard,
+    status: newStatus,
+    currentPlayer: player,
+  } as Game;
 };
+
 export const updateBoard = (position: number) => (game: Game) => {
   const newBoard = [...game.board];
   newBoard[position] = game.currentPlayer;
@@ -50,4 +117,10 @@ const switchPlayer = (game: Game): Player => {
 };
 export const getWinner = (game: Game) => {
   return "X" as Player;
+};
+export const setWinner = (player: Player) => (game: Game) => {
+  return { ...game, currentPlayer: player, status: "WON" };
+};
+export const getGameStatus = (game: Game) => {
+  return game.status;
 };
--- test/index.test.ts
+++ test/index.test.ts
@@ -4,103 +4,118 @@ import {
   createGame,
   getBoard,
   getCurrentPlayer,
+  getGameStatus,
   getPlayers,
   getWinner,
   play,
-  updateBoard,
-} from "../src/index";
+} from '../src/index'
 
-describe("Juego", () => {
-  let game: Game;
+describe('Juego', () => {
+  let game: Game
   beforeAll(() => {
-    game = createGame();
-  });
+    game = createGame()
+  })
 
-  test("Game debe estar definido", () => {
-    expect(game).toBeDefined();
-  });
+  test('Game debe estar definido', () => {
+    expect(game).toBeDefined()
+  })
 
-  test("Juego debe tener el jugador X y el jugador O", () => {
-    const players: Player[] = getPlayers(game);
-    expect(players.length).toBe(2);
-    expect(players).toContain("X");
-    expect(players).toContain("O");
-  });
+  test('Juego debe tener el jugador X y el jugador O', () => {
+    const players: Player[] = getPlayers(game)
+    expect(players.length).toBe(2)
+    expect(players).toContain('X')
+    expect(players).toContain('O')
+  })
 
-  test("Juego debe tener un tablero con nueve posiciones", () => {
-    const board = getBoard(game);
-    expect(board).toBeDefined();
-    expect(board.length).toBe(9);
-  });
+  test('Juego debe tener un tablero con nueve posiciones', () => {
+    const board = getBoard(game)
+    expect(board).toBeDefined()
+    expect(board.length).toBe(9)
+  })
 
-  test("El Juego comienza con el jugador X", () => {
-    const currentPlayer = getCurrentPlayer(game);
-    expect(currentPlayer).toBeDefined();
-    expect(currentPlayer).toBe("X");
-  });
+  test('El Juego comienza con el jugador X', () => {
+    const currentPlayer = getCurrentPlayer(game)
+    expect(currentPlayer).toBeDefined()
+    expect(currentPlayer).toBe('X')
+  })
 
-  test("Deberia cambiar de jugador despues de una jugada valida", () => {
-    const position = 0;
-    const newGame = play(position)(game);
+  test('Deberia cambiar de jugador despues de una jugada valida', () => {
+    const position = 0
+    const newGame = play(position)(game)
 
-    expect(getCurrentPlayer(newGame)).toBe("O");
-  });
+    expect(getCurrentPlayer(newGame)).toBe('O')
+  })
 
-  test("No deberia cambiar jugadores despues de una jugada invalida", () => {
-    const position = 9;
-    const newGame = play(position)(game);
-    expect(newGame).toEqual(game);
-    expect(getCurrentPlayer(newGame)).toBe("X");
-  });
+  test('No deberia cambiar jugadores despues de una jugada invalida', () => {
+    const position = 9
+    const newGame = play(position)(game)
+    expect(newGame).toEqual(game)
+    expect(getCurrentPlayer(newGame)).toBe('X')
+  })
 
-  test("No deberia jugar en una posicion ya ocupada", () => {
-    const position = 0;
-    const newGame = play(position)(game);
-    const duplicate = play(position)(newGame);
-    expect(duplicate).toEqual(newGame);
-  });
+  test('No deberia jugar en una posicion ya ocupada', () => {
+    const position = 0
+    const newGame = play(position)(game)
+    const duplicate = play(position)(newGame)
+    expect(duplicate).toEqual(newGame)
+  })
 
-  test("Deberia declarar un ganador si un jugador marca tres casillas en una fila", () => {
-    const gameStatus1 = play(0)(game);
-    const gameStatus2 = play(4)(gameStatus1);
-    const gameStatus3 = play(1)(gameStatus2);
-    const gameStatus4 = play(5)(gameStatus3);
-    const gameStatus5 = play(2)(gameStatus4);
-    const winner = getWinner(gameStatus5);
-    expect(winner).toBeDefined();
-    expect(winner).toBe("X");
-  });
+  test('Deberia declarar un ganador si un jugador marca tres casillas en una fila', () => {
+    const gameStatus1 = play(0)(game)
+    const gameStatus2 = play(4)(gameStatus1)
+    const gameStatus3 = play(1)(gameStatus2)
+    const gameStatus4 = play(5)(gameStatus3)
+    const gameStatus5 = play(2)(gameStatus4)
+    const winner = getWinner(gameStatus5)
+    expect(winner).toBeDefined()
+    expect(winner).toBe('X')
+  })
 
-  test("Deberia declarar un ganador si un jugador marca tres casillas verticales", () => {
-    const gameStatus1 = play(0)(game);
-    const gameStatus2 = play(1)(gameStatus1);
-    const gameStatus3 = play(3)(gameStatus2);
-    const gameStatus4 = play(4)(gameStatus3);
-    const gameStatus5 = play(6)(gameStatus4);
-    const winner = getWinner(gameStatus5);
-    expect(winner).toBeDefined();
-    expect(winner).toBe("X");
-  });
+  test('Deberia declarar un ganador si un jugador marca tres casillas verticales', () => {
+    const gameStatus1 = play(0)(game)
+    const gameStatus2 = play(1)(gameStatus1)
+    const gameStatus3 = play(3)(gameStatus2)
+    const gameStatus4 = play(4)(gameStatus3)
+    const gameStatus5 = play(6)(gameStatus4)
+    const winner = getWinner(gameStatus5)
+    expect(winner).toBeDefined()
+    expect(winner).toBe('X')
+  })
 
-  test("Deberia declarar un ganador si un jugador marca tres casillas en diagonal de izquierda a derecha", () => {
-    const gameStatus1 = play(6)(game);
-    const gameStatus2 = play(1)(gameStatus1);
-    const gameStatus3 = play(4)(gameStatus2);
-    const gameStatus4 = play(3)(gameStatus3);
-    const gameStatus5 = play(2)(gameStatus4);
-    const winner = getWinner(gameStatus5);
-    expect(winner).toBeDefined();
-    expect(winner).toBe("X");
-  });
+  test('Deberia declarar un ganador si un jugador marca tres casillas en diagonal de izquierda a derecha', () => {
+    const gameStatus1 = play(6)(game)
+    const gameStatus2 = play(1)(gameStatus1)
+    const gameStatus3 = play(4)(gameStatus2)
+    const gameStatus4 = play(3)(gameStatus3)
+    const gameStatus5 = play(2)(gameStatus4)
+    const winner = getWinner(gameStatus5)
+    expect(winner).toBeDefined()
+    expect(winner).toBe('X')
+  })
 
-  test("Deberia declarar un ganador si un jugador marca tres casillas en diagonal de derecha a izquierda", () => {
-    const gameStatus1 = play(0)(game);
-    const gameStatus2 = play(1)(gameStatus1);
-    const gameStatus3 = play(4)(gameStatus2);
-    const gameStatus4 = play(3)(gameStatus3);
-    const gameStatus5 = play(8)(gameStatus4);
-    const winner = getWinner(gameStatus5);
-    expect(winner).toBeDefined();
-    expect(winner).toBe("X");
-  });
-});
+  test('Deberia declarar un ganador si un jugador marca tres casillas en diagonal de derecha a izquierda', () => {
+    const gameStatus1 = play(0)(game)
+    const gameStatus2 = play(1)(gameStatus1)
+    const gameStatus3 = play(4)(gameStatus2)
+    const gameStatus4 = play(3)(gameStatus3)
+    const gameStatus5 = play(8)(gameStatus4)
+    const winner = getWinner(gameStatus5)
+    expect(winner).toBeDefined()
+    expect(winner).toBe('X')
+  })
+
+  test('Deberia declarar empate si todas las casillas estan ocupadas y no hay ganadores', () => {
+    const gameStatus1 = play(0)(game)
+    const gameStatus2 = play(1)(gameStatus1)
+    const gameStatus3 = play(2)(gameStatus2)
+    const gameStatus4 = play(3)(gameStatus3)
+    const gameStatus5 = play(4)(gameStatus4)
+    const gameStatus6 = play(5)(gameStatus5)
+    const gameStatus7 = play(6)(gameStatus6)
+    const gameStatus8 = play(7)(gameStatus7)
+    const gameStatus9 = play(8)(gameStatus8)
+    const status = getGameStatus(gameStatus9)
+    expect(status).toBeDefined()
+    expect(status).toBe('DRAW')
+  })
+})
